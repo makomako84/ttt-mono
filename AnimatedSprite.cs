adb012fac2c64f82899c4b6ca42642f66514b9ad
hanzzbrr <artur.galimsky@gmail.com>
2022-07-11 22:43:18 +0500

    examples

---
+++ AnimatedSprite.cs
@@ -0,0 +1,45 @@
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace ttt_mono
+{
+    public class AnimatedSprite
+    {
+        public Texture2D Texture { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        private int _currentFrame;
+        private int _totalFrames;
+
+        public AnimatedSprite(Texture2D texture, int rows, int columns)
+        {
+            Texture = texture;
+            Rows = rows;
+            Columns = columns;
+            _currentFrame = 0;
+            _totalFrames = Rows * Columns;
+        }
+
+        public void Update()
+        {
+            _currentFrame++;
+            if(_currentFrame == _totalFrames)
+                _currentFrame = 0;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            int width = Texture.Width / Columns;
+            int height = Texture.Height / Rows;
+            int row = _currentFrame / Columns;
+            int column = _currentFrame % Columns;
+        
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, height);
+        
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
+        }
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -7,9 +7,17 @@ namespace ttt_mono
     public class Game1 : Game
     {
         private Texture2D _xTexture;
+        private Texture2D _oTexture;
+        private Texture2D _eTexture;
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private AnimatedSprite _animatedSprite;
+        private Model _model;
+
+        private Matrix world = Matrix.CreateTranslation(new Vector3(0, 0, 0));
+        private Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 10), new Vector3(0, 0, 0), Vector3.UnitY);
+        private Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 480f, 0.1f, 100f);
 
         public Game1()
         {
@@ -31,6 +39,13 @@ namespace ttt_mono
 
             // TODO: use this.Content to load your game content here
             _xTexture = Content.Load<Texture2D>("x");
+            _eTexture = Content.Load<Texture2D>("e");
+            _oTexture = Content.Load<Texture2D>("o");
+
+            var texture = Content.Load<Texture2D>("SmileyWalk");
+            _animatedSprite = new AnimatedSprite(texture, 4, 4);
+
+            _model = Content.Load<Model>("box");
             
         }
 
@@ -40,18 +55,47 @@ namespace ttt_mono
                 Exit();
 
             // TODO: Add your update logic here
+            _animatedSprite.Update();
 
             base.Update(gameTime);
+        }
+
+        private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = projection;
+                }
+        
+                mesh.Draw();
+            }
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            // batch - партия, серия
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
             _spriteBatch.Draw(_xTexture, new Vector2(0, 0), Color.White);
             _spriteBatch.End();
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_eTexture, new Vector2(100, 0), Color.White);
+            _spriteBatch.End();
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_oTexture, new Vector2(200, 0), Color.White);
+            _spriteBatch.End();
+
+            _animatedSprite.Draw(_spriteBatch, new Vector2(400, 200));
+
+            DrawModel(_model, world, view, projection);
 
             base.Draw(gameTime);
         }
